071b1732c6ee5b48b7bb4ecfbe57ed0ff327e10f
Cristhian Gómez De la Cruz <89089619+GabrielGomezDlc@users.noreply.github.com>
2024-10-08 22:16:30 -0500

    feat(eureka-server): first service creation

---
+++ eureka-server/src/test/java/com/kora/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kora.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
